84b852b856387b0eaccfe25bddd5631c9931612f
Aswin K O <aswinko479@gmail.com>
2024-03-29 12:09:05 +0530

    created message model and conversation model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,31 +1,34 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        minLength: 6
+      type: String,
+      required: true,
+      minLength: 6,
     },
     gender: {
-        type: String,
-        required: true,
-        enum: ["Male", "Female"]
+      type: String,
+      required: true,
+      enum: ["Male", "Female"],
     },
     profilePic: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
-})
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
