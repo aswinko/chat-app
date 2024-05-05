5bb0cfb53f80b5880225eb88b4f73261b5f5766c
Aswin K O <aswinko479@gmail.com>
2024-05-05 13:12:05 +0530

    Project ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -5,7 +6,6 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app, server} from "./socket/socket.js"
 
@@ -19,9 +19,17 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT|| 5000;
 
+const __dirname = path.resolve()
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
   connectToMongoDB();
